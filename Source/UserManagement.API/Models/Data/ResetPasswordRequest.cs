16512ceb225d7da087d738fa22a21f71ad108a27
iamstan13y <iamstan13y@gmail.com>
2022-07-06 09:53:50 +0200

    User Mgmt Data.

---
+++ Source/UserManagement.API/Models/Data/ResetPasswordRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace UserManagement.API.Models.Data
+{
+    public class ResetPasswordRequest
+    {
+        public string? UserEmail { get; set; }
+        public string? OtpCode { get; set; }
+        public string? NewPassword { get; set; }
+    }
+}
