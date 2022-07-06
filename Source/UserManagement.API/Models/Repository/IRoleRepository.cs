97b7af291f193ad39396ddc4aa1efa13283cd649
iamstan13y <iamstan13y@gmail.com>
2022-07-06 09:55:36 +0200

    Role Repository Interface.

---
+++ Source/UserManagement.API/Models/Repository/IRoleRepository.cs
@@ -0,0 +1,12 @@
+﻿using ModelLibrary;
+
+namespace UserManagement.API.Models.Repository
+{
+    public interface IRoleRepository
+    {
+        Task<Result<Role>> AddAsync(Role role);
+        Task<Result<Role>> UpdateAsync(Role role);
+        Task<Result<IEnumerable<Role>>> GetAllAsync();
+        Task<Result<Role>> GetByIdAsync(int id);
+    }
+}
