a7f44e7ccd0e58e50f949b6b864b7ab9b1977427
iamstan13y <iamstan13y@gmail.com>
2022-07-06 09:52:58 +0200

    User Mgmt Data.

---
+++ Source/UserManagement.API/Models/Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using UserManagement.API.Enums;
+
+namespace UserManagement.API.Models.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Account>? Accounts { get; set; }
+        public DbSet<Role>? Roles { get; set; }
+        public DbSet<GeneratedCode>? GeneratedCodes { get; set; }
+    }
+}
