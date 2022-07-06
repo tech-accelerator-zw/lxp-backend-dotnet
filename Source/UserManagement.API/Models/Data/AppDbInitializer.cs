0fc4c24e36f8d51aef6ecada1a24db96a9f5fd1c
iamstan13y <iamstan13y@gmail.com>
2022-07-06 09:52:51 +0200

    User Mgmt Data.

---
+++ Source/UserManagement.API/Models/Data/AppDbInitializer.cs
@@ -0,0 +1,32 @@
+﻿using UserManagement.API.Enums;
+
+namespace UserManagement.API.Models.Data
+{
+    public class AppDbInitializer
+    {
+        public static void Seed(WebApplication app)
+        {
+            using var serviceScope = app.Services.GetService<IServiceScopeFactory>()!.CreateScope();
+
+            var context = serviceScope.ServiceProvider.GetService<ApplicationDbContext>();
+
+            if (!context!.Accounts!.Any())
+            {
+                context.Accounts!.Add(new Account
+                {
+                    Email = "admin@admin.com",
+                    RoleId = 1,
+                    UserName = "admin",
+                    FirstName = "Super",
+                    LastName = "Admin",
+                    Password = "$ju!137$V1$10000$Y6WVORfADjluJmwF0/bS0Vv9McwCQdj/h6HYf/omtIqBIchy",
+                    Status = Status.Verified,
+                    DateCreated = DateTime.Now,
+                    PhoneNumber = string.Empty
+                });
+
+                context.SaveChanges();
+            }
+        }
+    }
+}
