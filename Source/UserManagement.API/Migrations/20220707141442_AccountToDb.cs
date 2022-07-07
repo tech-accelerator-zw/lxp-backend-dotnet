8b86285374342f167a020ed210c07097a43272fc
iamstan13y <iamstan13y@gmail.com>
2022-07-07 16:16:04 +0200

    Account DB migrations.

---
+++ Source/UserManagement.API/Migrations/20220707141442_AccountToDb.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace UserManagement.API.Migrations
+{
+    public partial class AccountToDb : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "FirstName",
+                table: "Accounts");
+
+            migrationBuilder.DropColumn(
+                name: "LastName",
+                table: "Accounts");
+
+            migrationBuilder.DropColumn(
+                name: "UserName",
+                table: "Accounts");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DateModified",
+                table: "Accounts",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DateModified",
+                table: "Accounts");
+
+            migrationBuilder.AddColumn<string>(
+                name: "FirstName",
+                table: "Accounts",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "LastName",
+                table: "Accounts",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "UserName",
+                table: "Accounts",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+    }
+}
