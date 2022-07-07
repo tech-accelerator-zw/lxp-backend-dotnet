d70e940ebd63a41302cdded64986dbd33a015987
iamstan13y <iamstan13y@gmail.com>
2022-07-07 16:13:59 +0200

    Account fixes WIP.

--- Source/UserManagement.API/Models/Account.cs
+++ Source/UserManagement.API/Models/Account.cs
@@ -7,7 +7,7 @@ namespace UserManagement.API.Models
     public class Account
     {
         [Key]
-        public int UserId { get; set; }
+        public int Id { get; set; }
         public string? Email { get; set; }
         public string? PhoneNumber { get; set; }
         public string? Password { get; set; }
