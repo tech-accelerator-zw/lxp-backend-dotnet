a73bd95686c2a6c0fe7fe617d5428d6931327069
iamstan13y <iamstan13y@gmail.com>
2022-07-08 15:44:51 +0200

    Student request.

---
+++ Source/UserManagement.API/Models/Data/StudentRequest.cs
@@ -0,0 +1,11 @@
+﻿namespace UserManagement.API.Models.Data
+{
+    public class StudentRequest
+    {
+        public int UserId { get; set; }
+        public string? FirstNames { get; set; }
+        public string? LastName { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string? Otp { get; set; }
+    }
+}
