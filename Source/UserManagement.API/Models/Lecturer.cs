8b34e9763159793f54dec58f47fe915491c480a2
iamstan13y <iamstan13y@gmail.com>
2022-07-09 01:36:23 +0200

    Lecturer model & DB migrations.

---
+++ Source/UserManagement.API/Models/Lecturer.cs
@@ -0,0 +1,10 @@
+﻿namespace UserManagement.API.Models
+{
+    public class Lecturer
+    {
+        public int Id { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public string? Title { get; set; }
+    }
+}
