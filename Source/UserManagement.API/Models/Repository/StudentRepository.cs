e97f8d68e3156ee2b71c7ed8b5fa70c4b97652ae
iamstan13y <iamstan13y@gmail.com>
2022-07-08 10:05:15 +0200

    Student repository interface & DI.

---
+++ Source/UserManagement.API/Models/Repository/StudentRepository.cs
@@ -0,0 +1,17 @@
+﻿using ModelLibrary;
+
+namespace UserManagement.API.Models.Repository
+{
+    public class StudentRepository : IStudentRepository
+    {
+        public Task<Result<Student>> AddAsync(Student student)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Result<IEnumerable<Student>>> GetAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
