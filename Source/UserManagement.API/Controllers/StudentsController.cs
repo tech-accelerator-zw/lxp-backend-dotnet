ac14cf2c5bbec0878f245131dcf357ce1fc15c9c
iamstan13y <iamstan13y@gmail.com>
2022-07-09 00:16:49 +0200

    Student registration process.

---
+++ Source/UserManagement.API/Controllers/StudentsController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using UserManagement.API.Models.Data;
+using UserManagement.API.Models.Repository;
+
+namespace UserManagement.API.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class StudentsController : ControllerBase
+    {
+        private readonly IStudentRepository _studentRepository;
+
+        public StudentsController(IStudentRepository studentRepository)
+        {
+            _studentRepository = studentRepository;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Add(StudentRequest studentRequest)
+        {
+            var result = await _studentRepository.AddAsync(studentRequest); 
+
+            if(!result.Success) return BadRequest(result);
+
+            return Ok(result);
+        }
+    }
+}
