5cfdc38cb1f7bb7989ba325d097c2a1eafb19781
iamstan13y <iamstan13y@gmail.com>
2022-07-08 09:54:26 +0200

    Student->Account relationship.

--- Source/UserManagement.API/Models/Student.cs
+++ Source/UserManagement.API/Models/Student.cs
@@ -1,13 +1,18 @@
-﻿namespace UserManagement.API.Models
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace UserManagement.API.Models
 {
     public class Student
     {
         public int Id { get; set; }
+        public int UserId { get; set; }
         public string? FirstNames { get; set; }
         public string? LastName { get; set; }
         public DateTime DateOfBirth { get; set; }
-        public bool IsActive { get; set; } = false;
+        public bool IsActive { get; set; } = true;
         public DateTime DateCreated { get; set; }
         public DateTime DateModified { get; set; }
+        [ForeignKey("UserId")]
+        public Account? Account { get; set; }
     }
 }
