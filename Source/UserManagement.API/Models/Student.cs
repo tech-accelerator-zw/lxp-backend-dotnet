611fcf5648c4f775f561a40c05bfe344c9e21455
iamstan13y <iamstan13y@gmail.com>
2022-07-08 09:37:44 +0200

    Student model & DB migrations.

---
+++ Source/UserManagement.API/Models/Student.cs
@@ -0,0 +1,13 @@
+﻿namespace UserManagement.API.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string? FirstNames { get; set; }
+        public string? LastName { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public bool IsActive { get; set; } = false;
+        public DateTime DateCreated { get; set; }
+        public DateTime DateModified { get; set; }
+    }
+}
