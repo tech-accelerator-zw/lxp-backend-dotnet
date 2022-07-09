345dc4e8a4144880bfed96d171125557031ea9ca
iamstan13y <iamstan13y@gmail.com>
2022-07-09 02:22:54 +0200

    Updated Lecturer model & DB migrations.

---
+++ Source/UserManagement.API/Migrations/20220709002207_LecturerUserAccountToDb.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace UserManagement.API.Migrations
+{
+    public partial class LecturerUserAccountToDb : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DateCreated",
+                table: "Lecturers",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DateModified",
+                table: "Lecturers",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsActive",
+                table: "Lecturers",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<int>(
+                name: "UserId",
+                table: "Lecturers",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Lecturers_UserId",
+                table: "Lecturers",
+                column: "UserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Lecturers_Accounts_UserId",
+                table: "Lecturers",
+                column: "UserId",
+                principalTable: "Accounts",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Lecturers_Accounts_UserId",
+                table: "Lecturers");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Lecturers_UserId",
+                table: "Lecturers");
+
+            migrationBuilder.DropColumn(
+                name: "DateCreated",
+                table: "Lecturers");
+
+            migrationBuilder.DropColumn(
+                name: "DateModified",
+                table: "Lecturers");
+
+            migrationBuilder.DropColumn(
+                name: "IsActive",
+                table: "Lecturers");
+
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "Lecturers");
+        }
+    }
+}
