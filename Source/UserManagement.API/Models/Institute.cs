fccfb63369dbf23f98bf906cfd4ca2439d2177db
iamstan13y <iamstan13y@gmail.com>
2022-07-10 11:35:07 +0200

    Institue model & DB Migrations.

---
+++ Source/UserManagement.API/Models/Institute.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace UserManagement.API.Models
+{
+    public class Institute
+    {
+        public int Id { get; set; }
+        public string? RegisteredName { get; set; }
+        public string? LogoUrl { get; set; }
+        public bool IsActive { get; set; } = true;
+        public DateTime DateCreated { get; set; }
+        public DateTime DateModified { get; set; }
+        [ForeignKey("UserId")]
+        public Account? Account { get; set; }
+    }
+}
