e9de0c993c5a2399d1a4203b591d72bb748c1849
iamstan13y <iamstan13y@gmail.com>
2022-07-10 11:45:25 +0200

    Institute Repository interface & DI.

---
+++ Source/UserManagement.API/Models/Data/InstituteRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace UserManagement.API.Models.Data
+{
+    public class InstituteRequest
+    {
+        public int UserId { get; set; }
+        public string? RegisteredName { get; set; }
+        public string? LogoUrl { get; set; }
+    }
+}
