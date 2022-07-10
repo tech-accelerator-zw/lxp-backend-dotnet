e12f5e62fabd0762eef4b696855433fdbc3434a4
iamstan13y <iamstan13y@gmail.com>
2022-07-10 11:45:44 +0200

    Institute Repository interface & DI.

---
+++ Source/UserManagement.API/Models/Repository/InstituteRepository.cs
@@ -0,0 +1,18 @@
+﻿using ModelLibrary;
+using UserManagement.API.Models.Data;
+
+namespace UserManagement.API.Models.Repository
+{
+    public class InstituteRepository : IInstituteRepository
+    {
+        public Task<Result<Institute>> AddAsync(InstituteRequest request)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Result<IEnumerable<Institute>>> GetAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
