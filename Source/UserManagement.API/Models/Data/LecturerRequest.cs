5782d5918001cec8509432fb2337ef2314381d36
iamstan13y <iamstan13y@gmail.com>
2022-07-10 11:22:10 +0200

    Add Lecturer implementation.

--- Source/UserManagement.API/Models/Data/LecturerRequest.cs
+++ Source/UserManagement.API/Models/Data/LecturerRequest.cs
@@ -2,6 +2,10 @@
 {
     public class LecturerRequest
     {
-
+        public int UserId { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public string? Title { get; set; }
+        public string? Otp { get; set; }
     }
 }
