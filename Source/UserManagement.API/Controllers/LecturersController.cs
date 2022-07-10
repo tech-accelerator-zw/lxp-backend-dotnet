1e772e0d1e25d689a88e6894e5b6861727b9ab2b
iamstan13y <iamstan13y@gmail.com>
2022-07-10 11:22:03 +0200

    Add Lecturer implementation.

---
+++ Source/UserManagement.API/Controllers/LecturersController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using UserManagement.API.Models.Data;
+using UserManagement.API.Models.Repository;
+
+namespace UserManagement.API.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class LecturersController : ControllerBase
+    {
+        private readonly ILecturerRepository _lecturerRepository;
+
+        public LecturersController(ILecturerRepository lecturerRepository)
+        {
+            _lecturerRepository = lecturerRepository;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Add(LecturerRequest request)
+        {
+            var result = await _lecturerRepository.AddAsync(request);
+            if(!result.Success) return BadRequest(result);
+
+            return Ok(result);
+        }
+    }
+}
