dbaf176d25101a80c69353f7e7f6a4c11b2c4c87
iamstan13y <iamstan13y@gmail.com>
2022-07-10 11:57:41 +0200

    Add Institute Implementation.

--- Source/UserManagement.API/Models/Data/InstituteRequest.cs
+++ Source/UserManagement.API/Models/Data/InstituteRequest.cs
@@ -5,5 +5,6 @@
         public int UserId { get; set; }
         public string? RegisteredName { get; set; }
         public string? LogoUrl { get; set; }
+        public string? Otp { get; set; }
     }
 }
