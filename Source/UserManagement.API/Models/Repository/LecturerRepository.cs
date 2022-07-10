854e517e8223c80657eb61917fb116340ee81181
iamstan13y <iamstan13y@gmail.com>
2022-07-10 11:09:05 +0200

    Lecturer Repository interface & DI.

---
+++ Source/UserManagement.API/Models/Repository/LecturerRepository.cs
@@ -0,0 +1,18 @@
+﻿using ModelLibrary;
+using UserManagement.API.Models.Data;
+
+namespace UserManagement.API.Models.Repository
+{
+    public class LecturerRepository : ILecturerRepository
+    {
+        public Task<Result<Lecturer>> AddAsync(LecturerRequest request)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Result<IEnumerable<Lecturer>>> GetAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
